80ae6878568c316d0f6f78a6432fb0cc3995e230
Chikee Sun <royallor@163.com>
2024-02-08 22:06:26 +0800

    fix some bugs

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -15,7 +15,7 @@ keymap.set("n", "<leader>sv", "<C-w>v")
 keymap.set("n", "<leader>sh", "<C-w>s")
 
 -- no highlight
-keymap.set("n", "<leader>nh", "nohl<CR>")
+keymap.set("n", "<leader>nh", ":nohl<CR>")
 
 ------ plugins --------
 -- nvim-tree
